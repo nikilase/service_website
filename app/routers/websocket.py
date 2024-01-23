c1be8a843effadbebc041c4f320829738762cc50
nikilase <niklas.eichenberg@tu-ilmenau.de>
2024-01-23 21:39:31 +0100

    Actual implementation of websocket button

--- app/routers/websocket.py
+++ app/routers/websocket.py
@@ -9,12 +9,26 @@ router = APIRouter()
 
 @router.websocket("/live_log/{service}")
 async def websocket_endpoint(websocket: WebSocket, service: str):
-
     await websocket.accept()
+    log_size = 20
+
+    async def read_websocket(ws: WebSocket):
+        nonlocal log_size
+        # ws.iter_json() endlessly waits for messages
+        async for message in ws.iter_json():
+            if message["type"] == "switch_log_size":
+                print("Received switch_log_size")
+                if log_size == 20:
+                    log_size = 40
+                else:
+                    log_size = 20
+
+    task = asyncio.create_task(read_websocket(websocket))
+
     try:
         while True:
             await asyncio.sleep(1)
-            logs = get_last_logs(service).decode("UTF-8")
+            logs = get_last_logs(service, log_size).decode("UTF-8")
             log_txt = ""
             for log in logs.split("\n"):
                 if "ERROR" in log:
@@ -28,6 +42,7 @@ async def websocket_endpoint(websocket: WebSocket, service: str):
 
             await websocket.send_text(log_txt)
     except Exception as e:
-        print(e)
+        print(f"Websocket Exception: {e}")
     finally:
-        await websocket.close()
+        task.cancel()
+
